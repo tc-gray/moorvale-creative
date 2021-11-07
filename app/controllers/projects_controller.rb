5bc45953b56fde8eee247350c33137fb93d2ddc6
Tegan Cadney-Gray <tegan.cadney@gmail.com>
2021-11-07 12:18:36 +0000

    create path and form added

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,7 +9,25 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
 
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      # redirect_to project_path(@project)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
+
+  def project_params
+    params.require(:project).permit(:title, :bio, :content, :current)
+  end
 
   def find_project
     @project = Project.find(params[:id])
