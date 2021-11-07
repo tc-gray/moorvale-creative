3708abdc4ccd00dca12f281ce678a0ef80139e55
Tegan Cadney-Gray <tegan.cadney@gmail.com>
2021-11-07 11:29:04 +0000

    actually saved the file

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,5 @@
 class PagesController < ApplicationController
-<<<<<<< HEAD
   # skip_before_action :authenticate_user!, only: [ :home ]
-=======
-  # skip_before_action :authenticate_user!, only: [ :home, :contact ]
->>>>>>> 9d5877763e8de0e346b7a7b0dd8782fb2d716d20
 
   def home
   end
